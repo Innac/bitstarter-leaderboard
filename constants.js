4cb6b34db059f4663d790768b972e9ed1c4794a3
Innac <miskozverys@gmail.com>
2013-09-09 13:31:50 +0000

    New Coinbase button and edited info about Bitstarter

--- constants.js
+++ constants.js
@@ -25,12 +25,12 @@ var Constants = {
   FUNDING_TARGET: 10.00,
   FUNDING_UNIT_SYMBOL: "mBTC",
   FUNDING_SI_SCALE: 1000,
-  FUNDING_END_DATE: new Date("September 8, 2013"),
-  PRODUCT_NAME: "Product: Development Version",
-  PRODUCT_SHORT_DESCRIPTION: "One sentence description.",
+  FUNDING_END_DATE: new Date("September 30, 2013"),
+  PRODUCT_NAME: "Activfits: Development Version",
+  PRODUCT_SHORT_DESCRIPTION: "Activfits is an app that helps elder people stay active and healthy",
   TWITTER_USERNAME: "nodejs",
   TWITTER_TWEET: "This student crowdfunder looks interesting.",
-  COINBASE_PREORDER_DATA_CODE: "13b56883764b54e6ab56fef3bcc7229c",
+  COINBASE_PREORDER_DATA_CODE: "a6ce055b27c6bacc2a9f7f2627d5e024",
   days_left: function() {
       return Math.max(Math.ceil((this.FUNDING_END_DATE - new Date()) / (1000*60*60*24)), 0);
   }
