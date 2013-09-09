13e51633f15b69f0bba9225794314d237753425e
Innac <miskozverys@gmail.com>
2013-09-09 13:33:29 +0000

    Edited information

--- constants.js
+++ constants.js
@@ -1,25 +1,3 @@
-/*
-   Note that COINBASE_PREORDER_DATA_CODE is the button code from
-   coinbase.com/merchant_tools, used for the Preorder Button.  This is
-   different from the COINBASE_API key in .env file, which is from
-   coinbase.com/account/integrations
-
-   The button code can be shown publicly, while the API key should only be
-   included in a .env file and never shown publicly.  The former allows
-   people to send you money, the latter allows people to send money from
-   your account.
-
-   For FUNDING_UNIT_SYMBOL, we use mBTC to represent 1/1000 of a Bitcoin and
-   FUNDING_SI_SCALE for the corresponding multiplier.
-
-   Note that for FUNDING_UNIT_SYMBOL, in theory we could use the Thai Baht
-   symbol, but then we'd have to change the font.  If you use another
-   payment backend, you can substitute "$" for the dollar or use one of the
-   other currency symbols.
-
-    - https://en.bitcoin.it/wiki/Bitcoin_symbol#Existing_Unicode_symbol
-    - http://webdesign.about.com/od/localization/l/blhtmlcodes-cur.htm#codes
-*/
 var Constants = {
   APP_NAME: "Bitstarter", 
   FUNDING_TARGET: 10.00,
